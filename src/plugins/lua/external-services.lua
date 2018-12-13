557f8f8d478055e6e54dcab305e0b1dbe1c27024
Carsten Rosenberg <c.rosenberg@heinlein-support.de>
2018-12-13 21:39:58 +0100

    [Minor] External Services - adapted changes from antivirus.lua

--- src/plugins/lua/external-services.lua
+++ src/plugins/lua/external-services.lua
@@ -1,5 +1,6 @@
 --[[
 Copyright (c) 2016, Vsevolod Stakhov <vsevolod@highsecure.ru>
+Copyright (c) 2018, Carsten Rosenberg <c.rosenberg@heinlein-support.de>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -315,6 +316,41 @@ local function savapi_config(opts)
 
   rspamd_logger.errx(rspamd_config, 'cannot parse servers %s',
     savapi_conf['servers'])
+  return nil
+end
+
+local function kaspersky_config(opts)
+  local kaspersky_conf = {
+    scan_mime_parts = true;
+    scan_text_mime = false;
+    scan_image_mime = false;
+    product_id = 0,
+    log_clean = false,
+    timeout = 5.0,
+    retransmits = 1, -- use local files, retransmits are useless
+    cache_expire = 3600, -- expire redis in one hour
+    message = default_message,
+    tmpdir = '/tmp',
+    prefix = 'rs_ak',
+  }
+
+  kaspersky_conf = lua_util.override_defaults(kaspersky_conf, opts)
+
+  if not kaspersky_conf['servers'] then
+    rspamd_logger.errx(rspamd_config, 'no servers defined')
+
+    return nil
+  end
+
+  kaspersky_conf['upstreams'] = upstream_list.create(rspamd_config,
+      kaspersky_conf['servers'], 0)
+
+  if kaspersky_conf['upstreams'] then
+    return kaspersky_conf
+  end
+
+  rspamd_logger.errx(rspamd_config, 'cannot parse servers %s',
+      kaspersky_conf['servers'])
   return nil
 end
 
@@ -1001,7 +1037,28 @@ local function savapi_check(task, content, digest, rule)
     local upstream = rule.upstreams:get_upstream_round_robin()
     local addr = upstream:get_addr()
     local retransmits = rule.retransmits
-    local message_file = task:store_in_file(tonumber("0644", 8))
+    local fname = string.format('%s/%s.tmp',
+        rule.tmpdir, rspamd_util.random_hex(32))
+    local message_fd = rspamd_util.create_file(fname)
+
+    if not message_fd then
+      rspamd_logger.errx('cannot store file for savapi scan: %s', fname)
+      return
+    end
+
+    if type(content) == 'string' then
+      -- Create rspamd_text
+      local rspamd_text = require "rspamd_text"
+      content = rspamd_text.fromstring(content)
+    end
+    content:save_in_file(message_fd)
+
+    -- Ensure cleanup
+    task:get_mempool():add_destructor(function()
+      os.remove(fname)
+      rspamd_util.close_file(message_fd)
+    end)
+
     local vnames = {}
 
     -- Forward declaration for recursive calls
@@ -1030,7 +1087,8 @@ local function savapi_check(task, content, digest, rule)
 
     local function savapi_scan2_cb(err, data, conn)
       local result = tostring(data)
-      lua_util.debugm(N, task, "%s: got reply: %s", rule['type'], result)
+      lua_util.debugm(N, task, "%s: got reply: %s",
+        rule['type'], result)
 
       -- Terminal response - clean
       if string.find(result, '200') or string.find(result, '210') then
@@ -1051,7 +1109,8 @@ local function savapi_check(task, content, digest, rule)
         if not virus then
           virus = result:match "310%s(.*)%s+;.*;.*"
           if not virus then
-            rspamd_logger.errx(task, "%s: virus result unparseable: %s", rule['type'], result)
+            rspamd_logger.errx(task, "%s: virus result unparseable: %s",
+                rule['type'], result)
             return
           end
         end
@@ -1070,10 +1129,13 @@ local function savapi_check(task, content, digest, rule)
     local function savapi_greet2_cb(err, data, conn)
       local result = tostring(data)
       if string.find(result, '100 PRODUCT') then
-        lua_util.debugm(N, task, "%s: scanning file: %s", rule['type'], message_file)
-        conn:add_write(savapi_scan1_cb, {string.format('SCAN %s\n', message_file)})
+        lua_util.debugm(N, task, "%s: scanning file: %s",
+            rule['type'], fname)
+        conn:add_write(savapi_scan1_cb, {string.format('SCAN %s\n',
+            fname)})
       else
-        rspamd_logger.errx(task, '%s: invalid product id %s', rule['type'], rule['product_id'])
+        rspamd_logger.errx(task, '%s: invalid product id %s', rule['type'],
+            rule['product_id'])
         conn:add_write(savapi_fin_cb, 'QUIT\n')
       end
     end
@@ -1137,6 +1199,120 @@ local function savapi_check(task, content, digest, rule)
       return
     else
       savapi_check_uncached()
+    end
+  end
+end
+
+local function kaspersky_check(task, content, digest, rule)
+  local function kaspersky_check_uncached ()
+    local upstream = rule.upstreams:get_upstream_round_robin()
+    local addr = upstream:get_addr()
+    local retransmits = rule.retransmits
+    local fname = string.format('%s/%s.tmp',
+        rule.tmpdir, rspamd_util.random_hex(32))
+    local message_fd = rspamd_util.create_file(fname)
+    local clamav_compat_cmd = string.format("nSCAN %s\n", fname)
+
+    if not message_fd then
+      rspamd_logger.errx('cannot store file for kaspersky scan: %s', fname)
+      return
+    end
+
+    if type(content) == 'string' then
+      -- Create rspamd_text
+      local rspamd_text = require "rspamd_text"
+      content = rspamd_text.fromstring(content)
+    end
+    content:save_in_file(message_fd)
+
+    -- Ensure file cleanup
+    task:get_mempool():add_destructor(function()
+      os.remove(fname)
+      rspamd_util.close_file(message_fd)
+    end)
+
+
+    local function kaspersky_callback(err, data)
+      if err then
+        -- set current upstream to fail because an error occurred
+        upstream:fail()
+
+        -- retry with another upstream until retransmits exceeds
+        if retransmits > 0 then
+
+          retransmits = retransmits - 1
+
+          -- Select a different upstream!
+          upstream = rule.upstreams:get_upstream_round_robin()
+          addr = upstream:get_addr()
+
+          lua_util.debugm(N, task,
+              '%s [%s]: retry IP: %s', rule['symbol'], rule['type'], addr)
+
+          tcp.request({
+            task = task,
+            host = addr:to_string(),
+            port = addr:get_port(),
+            timeout = rule['timeout'],
+            callback = kaspersky_callback,
+            data = { clamav_compat_cmd },
+            stop_pattern = '\n'
+          })
+        else
+          rspamd_logger.errx(task,
+              '%s [%s]: failed to scan, maximum retransmits exceed',
+              rule['symbol'], rule['type'])
+          task:insert_result(rule['symbol_fail'], 0.0,
+              'failed to scan and retransmits exceed')
+        end
+
+      else
+        upstream:ok()
+        data = tostring(data)
+        local cached
+        lua_util.debugm(N, task, '%s [%s]: got reply: %s',
+            rule['symbol'], rule['type'], data)
+        if data == 'stream: OK' then
+          cached = 'OK'
+          if rule['log_clean'] then
+            rspamd_logger.infox(task, '%s [%s]: message or mime_part is clean',
+                rule['symbol'], rule['type'])
+          else
+            lua_util.debugm(N, task, '%s [%s]: message or mime_part is clean',
+                rule['symbol'], rule['type'])
+          end
+        else
+          local vname = string.match(data, ': (.+) FOUND')
+          if vname then
+            yield_result(task, rule, vname)
+            cached = vname
+          else
+            rspamd_logger.errx(task, 'unhandled response: %s', data)
+            task:insert_result(rule['symbol_fail'], 0.0, 'unhandled response')
+          end
+        end
+        if cached then
+          save_av_cache(task, digest, rule, cached)
+        end
+      end
+    end
+
+    tcp.request({
+      task = task,
+      host = addr:to_string(),
+      port = addr:get_port(),
+      timeout = rule['timeout'],
+      callback = kaspersky_callback,
+      data = { clamav_compat_cmd },
+      stop_pattern = '\n'
+    })
+  end
+
+  if need_av_check(task, content, rule) then
+    if check_av_cache(task, digest, rule, kaspersky_check_uncached) then
+      return
+    else
+      kaspersky_check_uncached()
     end
   end
 end
@@ -1826,6 +2002,10 @@ local av_types = {
   savapi = {
     configure = savapi_config,
     check = savapi_check
+  },
+  kaspersky = {
+    configure = kaspersky_config,
+    check = kaspersky_check
   },
   dcc = {
     configure = dcc_config,
